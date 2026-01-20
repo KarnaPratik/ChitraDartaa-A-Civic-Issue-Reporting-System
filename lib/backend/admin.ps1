97af7784d7ff997fca893344f0343a8eaec0b4f6
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2026-01-20 22:18:49 +0545

    sabai

---
+++ lib/backend/admin.ps1
@@ -0,0 +1,17 @@
+# Define the payload
+$body = @{
+    username = "mahesh_admin"
+    email    = "panta@gmail.com"
+    password = "12345678"
+    is_admin = $true
+} | ConvertTo-Json
+
+# Execute the POST request
+$params = @{
+    Uri         = "http://127.0.0.1:6969/auth/signup"
+    Method      = "POST"
+    Headers     = @{ "Content-Type" = "application/json; charset=utf-8" }
+    Body        = $body
+}
+
+Invoke-RestMethod @params
